aeb694b52370cf2b90107ef33818c565ddda2057
git_username <git_email>
2023-07-08 17:30:45 +0200

    feat(completed RQTS)

--- src/EventManager/RQTSEvent.ts
+++ src/EventManager/RQTSEvent.ts
@@ -1,12 +1,16 @@
 export type Topic = string;
 
-export interface Data {
-	id: string;
+export type Data = {
 	[key: string]: unknown;
-}
+	id: string;
+};
 
 export type RQTSEvent = {
 	topic: Topic;
 	data?: Data;
 };
 
+export const createRQTSEvent = (topic: Topic, data?: Data): RQTSEvent => ({
+	topic,
+	data,
+});
