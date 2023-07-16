6e351592ff01ef182555cf3c03d2fe9b0c2e054e
git_username <git_email>
2023-07-16 11:47:30 +0200

    feat(changing RQTS Data content)

--- src/EventManager/RQTSEvent.ts
+++ src/EventManager/RQTSEvent.ts
@@ -1,13 +1,21 @@
+import {v4 as uuidv4} from 'uuid';
+
 export type Topic = string;
 
-export type Data = Record<string, unknown> & { id: string }
+export type Data = object & { id: string }
 
 export type RQTSEvent = {
     topic: Topic;
     data?: Data;
 };
 
-export const createRQTSEvent = <T>(topic: Topic, data?: Data): RQTSEvent => ({
-    topic,
-    data,
-});
+export const createRQTSEvent = <T extends object>(topic: Topic, data?: T): RQTSEvent => {
+    return {
+        topic,
+        data: data
+            ? 'id' in data
+                ? data as Data
+                : {...data, id: uuidv4()}
+            : data,
+    };
+}
