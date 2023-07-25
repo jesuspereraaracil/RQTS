3a9cca523f007dc169e6430e614fdcfbe8a65e3c
JPereraAracil <jpereraaracil@gmail.com>
2023-07-25 18:48:38 +0200

    feat(removing lib generated folder)

--- lib/EventManager/RQTSEvent.d.ts
+++
@@ -1,6 +0,0 @@
-export type Topic = string;
-export type RQTSEvent = {
-    topic: Topic;
-    data?: unknown;
-};
-//# sourceMappingURL=RQTSEvent.d.ts.map
--- lib/index.d.ts
+++
@@ -1,3 +0,0 @@
-import { RQTSEventManager } from './EventManager/RQTSEventManager';
-export { RQTSEventManager };
-//# sourceMappingURL=index.d.ts.map
