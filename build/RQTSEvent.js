d8aee35622c3b9575e19af5c78d1ed206968cdb4
JPereraAracil <jpereraaracil@gmail.com>
2023-12-16 19:10:31 +0100

    feat(changing to bun)

--- build/RQTSEvent.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.createRQTSEvent = void 0;
-var createRQTSEvent = function (topic, data) { return ({
-    topic: topic,
-    data: data
-}); };
-exports.createRQTSEvent = createRQTSEvent;
