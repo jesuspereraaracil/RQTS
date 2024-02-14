66899af9d5898cc82ecbc9981d738d6d5c518ad2
JPereraAracil <jpereraaracil@gmail.com>
2024-02-14 20:53:28 +0100

    feat(making data from events optional)

--- src/functions.ts
+++ src/functions.ts
@@ -1,26 +1,26 @@
 import { createRQTSEvent, type RQTSEvent, type Topic } from './RQTSEvent'
 import { RQTSTubeManager } from './RQTSTubeManager'
 
-export const emit = <T>(topic: Topic, data: T): void => {
+export const emit = <T>(topic: Topic, data?: T): void => {
   const event = createRQTSEvent(topic, data)
   RQTSTubeManager.getInstance().publish(event)
 }
 
-export const receive = <T>(topic: Topic, handler: (data: T) => Promise<void>): string => {
+export const receive = <T>(topic: Topic, handler: (data?: T) => Promise<void>): string => {
   const onEvent = (event: RQTSEvent<T>): void => {
     void handler(event.data)
   }
   return RQTSTubeManager.getInstance().subscribeTo<T>(topic, onEvent)
 }
 
-export const receiveFirst = <T>(topic: Topic, handler: (data: T) => Promise<void>): string => {
+export const receiveFirst = <T>(topic: Topic, handler: (data?: T) => Promise<void>): string => {
   const onEvent = (event: RQTSEvent<T>): void => {
     void handler(event.data)
   }
   return RQTSTubeManager.getInstance().subscribeFirst<T>(topic, onEvent)
 }
 
-export const receiveAll = <T>(handler: (data: T) => Promise<void>): string => {
+export const receiveAll = <T>(handler: (data?: T) => Promise<void>): string => {
   const onEvent = (event: RQTSEvent<T>): void => {
     void handler(event.data)
   }
